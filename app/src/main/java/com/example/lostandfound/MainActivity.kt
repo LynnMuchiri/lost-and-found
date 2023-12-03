2de3f6dafc6f50be02725a8ad60f2b6ce65c7e97
BryanLwaya <98429501+BryanLwaya@users.noreply.github.com>
2023-12-04 01:37:45 +0300

    final fixes

--- app/src/main/java/com/example/lostandfound/MainActivity.kt
+++ app/src/main/java/com/example/lostandfound/MainActivity.kt
@@ -28,15 +28,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-//        navController.addOnDestinationChangedListener { _, destination, _ ->
-//            if(destination.id == R.id.HomeActivity) {
-//                actionBar?.hide()
-////            bottomNavigationView.setVisibility(View.GONE);
-//            } else {
-//                actionBar?.show()
-////            bottomNavigationView.setVisibility(View.VISIBLE);
-//            }
-//        }
+
     }
 
 
@@ -60,8 +52,5 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
-
-
         }
-
 }
